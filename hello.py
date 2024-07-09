f9a24e8602d7cac4f211d22a23feaccc9dd1c1e0
LoicSteve <loikstevegmail.com>
2024-07-09 11:07:04 +0200

    adding initial layout python project

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# #This is a nin  sense code that generated the warning
+# var = 1
+# var = var
+
+# #Bad syntax
+# foo =
